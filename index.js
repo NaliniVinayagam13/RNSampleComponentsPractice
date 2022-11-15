00f53fb5b786e04c75518be2aecd8c793b0d4758
nalini.v <nalini.v@optisolbusiness.com>
2022-11-15 11:17:47 +0530

    TextInput Component example code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/components/TextComponentExample';
+import App from './src/components/TextInputComponentExample';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
