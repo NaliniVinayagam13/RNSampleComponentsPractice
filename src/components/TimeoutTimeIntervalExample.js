fdc2b6122a61dad8afa2cb25884f9acd202c8753
nalini.v <nalini.v@optisolbusiness.com>
2022-12-01 12:00:51 +0530

    Code Modified

--- src/components/TimeoutTimeIntervalExample.js
+++ src/components/TimeoutTimeIntervalExample.js
@@ -1,64 +1,97 @@
 
 import React, { Component } from 'react';
-import { View, Image, ImageBackground, StyleSheet, Text, SafeAreaView } from 'react-native'
+import { View, Image, ImageBackground, StyleSheet, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 export default class TimeoutTimeIntervalExample extends Component {
 
+  state = {
+    isImageVisible: true,
+    isShowText: true
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isImageVisible: !this.state.isImageVisible })
+      console.log("ComponentDid Mount New is called -- isImgVisible: " + this.state.isImageVisible)
+    }, 1000)
+
+    const textTimer = setInterval(() => {
+      this.setState({ isShowText: !this.state.isShowText })
+      console.log("ComponentDid Mount New is called -- isShowText: " + this.state.isShowText)
+    }, 1000)
+
+  }
+
   render() {
-    let Join = setTimeout(function acknowledgement() {
-      alert('Congratulations, You have Done -- A');
-    }, 3000);
+    console.log("Render is called -- isImgVisible: " + this.state.isImageVisible + " -- isShowText: " + this.state.isShowText)
+    // let Join = setTimeout(function acknowledgement() {
+    //   alert('Congratulations, You have Done -- A');
+    // }, 3000);
 
-    let Join2 = setTimeout(() => {
-      alert('Congratulations, You have Done -- B');
-    }, 5000);
+    // let Join2 = setTimeout(() => {
+    //   alert('Congratulations, You have Done -- B');
+    // }, 5000);
 
-    function acknowledgement() {
-      alert('Congratulations, You have Done -- C');
-    }
+    // function acknowledgement() {
+    //   alert('Congratulations, You have Done -- C');
+    // }
 
-    let Join3 = setTimeout(acknowledgement, 7000);
+    // let Join3 = setTimeout(acknowledgement, 7000);
 
-    var isDisplayed = false;
-    var counter = 0;
-    function showOrHideText() {
-      counter++;
-      isDisplayed = !isDisplayed;
-      alert('Congratulations, You have Done -- '+counter);
+    // var isDisplayed = false;
+    // var counter = 0;
+    // function showOrHideText() {
+    //   counter++;
+    //   isDisplayed = !isDisplayed;
+    //   alert('Congratulations, You have Done -- '+counter);
 
-      if(counter == 10){
-        clearTimeout(showOrHide);
-      }
-    }
+    //   if(counter == 10){
+    //     clearTimeout(showOrHide);
+    //   }
+    // }
 
-    const showOrHide = setInterval(showOrHideText, 2000);
+    // const showOrHide = setInterval(showOrHideText, 2000);
 
     return (
 
-      { Join },
+      // { Join },
 
-      { Join2 },
+      // { Join2 },
 
-      { Join3 },
+      // { Join3 },
 
-      {showOrHide},
+      // {showOrHide},
 
       <SafeAreaView style={{ flex: 1 }}>
 
         <ImageBackground source={{ uri: 'https://reactnative-examples.com/wp-content/uploads/2022/03/winter-scaled.jpg' }}
           style={styles.backgroundImage}>
-          <Text style={styles.text}>
-            Example of ImageBackground and Image in React Native
-          </Text>
-          <Image
-            style={{ height: 100, width: 100,
-              margin: 30, display: isDisplayed?'none': 'flex'}}
-            source={{ uri: 'https://www.positronx.io/wp-content/uploads/2020/02/react-native-150x150-1.jpg' }}
 
-          />
+          <View style={{ backgroundColor: "#000000c0", margin: 25, padding: 5, width: 300, height: 175, alignContent: 'center'  }}>
+            {
+              this.state.isShowText ?
+                <Text style={styles.text}>
+                  Example of ImageBackground and Image in React Native
+                </Text> : null
+            }
+          </View>
 
-          <Image
-            style={styles.imageStyle}
-            source={require('../assets/images/optisol_logo.png')} />
+          {
+            this.state.isImageVisible ?
+              <Image
+                style={{
+                  height: 100, width: 100,
+                  margin: 30
+                }}
+                source={{ uri: 'https://www.positronx.io/wp-content/uploads/2020/02/react-native-150x150-1.jpg' }}
+
+              /> : null
+          }
+          <TouchableOpacity onPress={() => clearInterval(this.textTimer)}>
+            <Image
+              style={styles.imageStyle}
+              source={require('../assets/images/optisol_logo.png')}
+            />
+          </TouchableOpacity>
         </ImageBackground>
 
       </SafeAreaView>
@@ -76,12 +109,8 @@ const styles = StyleSheet.create({
     fontSize: 28,
     textAlign: 'center',
     color: 'white',
-    fontWeight: 'bold',
-    margin: 25,
-    paddingBottom: 15,
-    backgroundColor: "#000000c0",
-    padding: 5
-    
+    fontWeight: 'bold'
+
   },
 
   imageStyle: {
